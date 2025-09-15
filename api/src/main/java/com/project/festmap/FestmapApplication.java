542bee9383226d3b556fe7afe7a50c3beebde47f
Supzero-0 <mat.lecanu@gmail.com>
2025-09-15 15:57:29 +0200

    Initialization of the Project

---
+++ api/src/main/java/com/project/festmap/FestmapApplication.java
@@ -0,0 +1,13 @@
+package com.project.festmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestmapApplication.class, args);
+	}
+
+}
