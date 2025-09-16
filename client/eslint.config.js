b74769b69f2276e53452c69ab247f797a0f7024b
Supzero-0 <mat.lecanu@gmail.com>
2025-09-16 14:21:59 +0200

    Feature: Installation des packages Eslint et prettier + fichier configuration

---
+++ client/eslint.config.js
@@ -0,0 +1,14 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: { parser: tsParser },
+    plugins: { "@typescript-eslint": tseslint },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "no-console": ["warn", { allow: ["warn", "error"] }]
+    }
+  }
+];
