d598de8e002918d2fe6f9a369e59312f3c626386
Supzero-0 <mat.lecanu@gmail.com>
2025-09-16 15:59:43 +0200

    Feature: Modification du app.html pour ajouter un bouton de vérification de connexion à l'API

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('client');
+  title = 'FestMap';
+  result: any = null;
+  loading = false;
+
+  async testHealth() {
+    this.loading = true;
+    this.result = null;
+    try {
+      const response = await fetch('/api/health');
+      this.result = await response.json();
+    } catch (error: any) {
+      this.result = { statut: 'error', error: 'Error de connexion au serveur :' + error.message };
+    } finally {
+      this.loading = false;
+    }
+  }
 }
