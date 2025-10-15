7403d571f503f9a20310a14aa1bd560d348ccc2a
Supzero-0 <mat.lecanu@gmail.com>
2025-10-15 10:53:21 +0200

    feat: init map page + feature map and list (folders, routes and placeholders)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MapPage } from '../pages/map/map-page/map-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: MapPage }];
---
+++ client/src/features/festivals/components/festival-list/festival-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-festival-list',
+  imports: [],
+  templateUrl: './festival-list.html',
+  styleUrl: './festival-list.scss',
+})
+export class FestivalList {}
