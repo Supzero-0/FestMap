42e2687188f2d432e399fcbffe128bd3766d306f
Supzero-0 <mat.lecanu@gmail.com>
2026-01-06 14:39:21 +0100

    feat: create user table

---
+++ api/src/main/java/com/project/festmap/user/domain/UserRepository.java
@@ -0,0 +1,14 @@
+package com.project.festmap.user.domain;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
+
+  boolean existsByEmail(String email);
+}
