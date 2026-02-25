73d8cf1ff39c6dc325ce7f8e328e505032fc21b3
Supzero-0 <mat.lecanu@gmail.com>
2026-02-25 14:28:53 +0100

    chore: move feature and pages files inside src/app file

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -10,7 +10,7 @@ import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from '../features/auth/services/auth-interceptor';
+import { authInterceptor } from './features/auth/services/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- client/src/app/features/auth/services/auth-service.ts
+++ client/src/app/features/auth/services/auth-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../../environments/environment';
 import { User } from '../models/user-model';
 import { AuthResponse } from '../models/auth-response-model';
 import { AuthTokenService } from './auth-token-service';
