0a1e5952db0dce4257b83aa5b9d1c0c7e808d60a
Supzero-0 <mat.lecanu@gmail.com>
2026-03-02 11:16:03 +0100

    feat: create FestivalSidebar component with search functionality for festivals

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,13 +3,13 @@ import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { AuthModalComponent } from './features/auth/components/auth-modal/auth-modal';
 import { SidebarComponent } from './shared/components/sidebar/sidebar';
-import { FestivalList } from './features/festivals/components/festival-list/festival-list';
 import { FestivalMap } from './features/festivals/components/festival-map/festival-map';
+import { FestivalSidebar } from './features/festivals/components/festival-sidebar/festival-sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ToastModule, AuthModalComponent, SidebarComponent, FestivalList, FestivalMap],
+  imports: [ToastModule, AuthModalComponent, SidebarComponent, FestivalSidebar, FestivalMap],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [MessageService],
